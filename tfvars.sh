1a856f32f83fa20a80db0f6b80b27e10cb00d45f
Cameron Smith <cameron.ray.smith@gmail.com>
2022-10-31 12:07:38 -0400

    associate startup script name with instance name

--- tfvars.sh
+++ tfvars.sh
@@ -34,26 +34,33 @@ if ! command -v pass
 then
     echo "pass could not be found"
     echo "please see https://www.passwordstore.org/ for installation instructions"
-    echo "or set all variables using pass manually"
+    echo "or set all variables using pass manually and remove or comment this check"
     exit 1
 fi
 
 GITHUB_USERNAME=$(pass github_username)
-export GITHUB_ORG_NAME=$(pass github_org)
-export GITHUB_REPO_NAME=$(pass github_repo)
-STARTUP_SCRIPT_NAME="post-startup-script.sh"
+GITHUB_ORG_NAME=$(pass github_org)
+GITHUB_REPO_NAME=$(pass github_repo)
+TF_VAR_project="$(pass gcp_project)"
+TF_VAR_email="$(pass gcp_email)"
+TF_VAR_credentials_file="$(pass gcp_credentials_file)"
+TF_VAR_notebooks_name="$(pass gcp_notebooks_name)"
+export TF_VAR_project TF_VAR_email TF_VAR_credentials_file TF_VAR_notebooks_name GITHUB_ORG_NAME GITHUB_REPO_NAME
+
+STARTUP_SCRIPT_NAME="post-startup-script-$TF_VAR_notebooks_name.sh"
+# unset TF_VAR_project TF_VAR_email TF_VAR_credentials_file TF_VAR_notebooks_name GITHUB_ORG_NAME GITHUB_REPO_NAME
 
 cat template-post-startup-script.sh | \
 envsubst '${GITHUB_ORG_NAME} ${GITHUB_REPO_NAME}' > $STARTUP_SCRIPT_NAME
 
 get_startup_script_gist_id () {
-   echo "$(gh gist list | grep -m1 '.*post-startup-script.*' | cut -f1)"
+   echo "$(gh gist list | grep -m1 $STARTUP_SCRIPT_NAME | cut -f1)"
 }
 
 temp_gist_id=$(get_startup_script_gist_id)
 if [ -z "$temp_gist_id" ]; then
    echo "$temp_gist_id"
-   echo "no gist matching pattern 'post-startup-script'"
+   echo "no gist matching pattern $STARTUP_SCRIPT_NAME"
    echo "creating gist from $STARTUP_SCRIPT_NAME"
    gh gist create "$STARTUP_SCRIPT_NAME"
 else
@@ -64,13 +71,9 @@ fi
 
 STARTUP_SCRIPT_GIST_ID="$(get_startup_script_gist_id)"
 echo "startup script gist id: $STARTUP_SCRIPT_GIST_ID"
-TF_VAR_project="$(pass gcp_project)"
-TF_VAR_email="$(pass gcp_email)"
-TF_VAR_credentials_file="$(pass gcp_credentials_file)"
-TF_VAR_notebooks_name="$(pass gcp_notebooks_name)"
 TF_VAR_post_startup_script_url="https://gist.githubusercontent.com/$GITHUB_USERNAME/$STARTUP_SCRIPT_GIST_ID/raw/$STARTUP_SCRIPT_NAME"
-export TF_VAR_project TF_VAR_email TF_VAR_credentials_file TF_VAR_notebooks_name TF_VAR_post_startup_script_url
-# unset TF_VAR_project TF_VAR_email TF_VAR_credentials_file TF_VAR_notebooks_name TF_VAR_post_startup_script_url GITHUB_ORG_NAME GITHUB_REPO_NAME
+export TF_VAR_post_startup_script_url
+# unset TF_VAR_post_startup_script_url
 
 url_status=$(curl -s -o /dev/null -w "%{http_code}" "$TF_VAR_post_startup_script_url")
 echo "post startup script url: $TF_VAR_post_startup_script_url"
